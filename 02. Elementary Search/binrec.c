e34cff37034b782d5a8ab917a1fd946ba69c4632
Akhil <akhilrasheed16@gmail.com>
2022-08-11 09:59:45 +0530

    init

--- 02. Elementary Search/binrec.c
+++ 02. Elementary Search/binrec.c
@@ -1,67 +1,62 @@
-// binary search
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-#define X 10
-#define Y 100
-int count = 0;
 
-void binsearch(int arr[], int low, int high, int key) {
-    int mid = low + (high - low) / 2;
+int count;
+
+void binarySearch(int *arr, int key, int l, int h) {
     count++;
-    if (arr[mid] == key)
-        return;
-    else if (arr[mid] > key)
-        binsearch(arr, low, mid - 1, key);
-    else
-        binsearch(arr, mid + 1, high, key);
-}
-
-void helper(int arr[], int n, int key) {
-    count = 0;
-    binsearch(arr, 0, n - 1, key);
-}
-
-void binary(int choice) {
-    FILE *fp1, *fp2;
-    int i, j, temp, key, res, *arr;
-    srand(time(NULL));
-    if (choice == 1) {
-        fp1 = fopen("bestin.txt", "a");
-        fp2 = fopen("best.txt", "a");
-    } else if (choice == 2) {
-        fp1 = fopen("worstin.txt", "a");
-        fp2 = fopen("worst.txt", "a");
-    } else {
-        fp1 = fopen("avgin.txt", "a");
-        fp2 = fopen("avg.txt", "a");
+    if (l <= h) {
+        int mid = (h + l) / 2;
+        if (arr[mid] == key)
+            return;
+        else if (arr[mid] > key)
+            binarySearch(arr, key, l, mid - 1);
+        else
+            binarySearch(arr, key, mid + 1, h);
     }
-
-    for (i = X; i <= Y; i += 10) {
-        arr = (int *)malloc(sizeof(int) * i);
-        res = rand() % 100;
-        for (j = 0; j < i; j++) {
-            arr[j] = j + res;
-            fprintf(fp1, "%d ", arr[j]);
-        }
-        fprintf(fp1, "\n");
-        if (choice == 1) {
-            key = arr[(i - 1) / 2];
-        } else if (choice == 2) {
-            key = arr[i - 1];
-        } else {
-            temp = rand() % (i - 1);
-            key = arr[temp];
-        }
-        helper(arr, i, key);
-        fprintf(fp2, "%d\t%d\n", i, count);
-    }
-    fclose(fp1);
-    fclose(fp2);
 }
 
 void main() {
-    for (int i = 1; i <= 3; i++) {
-        binary(i);
+    int n, i, key;
+    int *arr;
+
+    srand(time(0));
+    FILE *b, *w, *a;
+    system("rm b.txt w.txt a.txt");
+
+    b = fopen("b.txt", "a");
+    w = fopen("w.txt", "a");
+    a = fopen("a.txt", "a");
+
+    for (n = 10; n < 1000; n += 10) {
+        arr = (int *)malloc(n * sizeof(int));
+
+        arr[0] = rand() % 100;
+        for (i = 1; i < n; i++) {
+            arr[i] = arr[i - 1] + rand() % 100;
+        }
+
+        // Best Case
+        count = 0;
+        key = arr[(n - 1) / 2] = 9999;
+        binarySearch(arr, key, 0, n - 1);
+        fprintf(b, "%d\t%d\n", n, count);
+
+        // Worst Case
+        count = 0;
+        key = 5555;
+        binarySearch(arr, key, 0, n - 1);
+        fprintf(w, "%d\t%d\n", n, count);
+
+        // Average Case
+        count = 0;
+        key = rand() % 100;
+        binarySearch(arr, key, 0, n - 1);
+        fprintf(a, "%d\t%d\n", n, count);
     }
+
+    fclose(b);
+    fclose(w);
+    fclose(a);
 }
